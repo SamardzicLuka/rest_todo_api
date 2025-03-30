94b16bc68d46a6b6fcfa51d12db8812e8de58467
Blicko <samardzicluka@proton.me>
2025-03-31 01:57:13 +0200

    Wrote app.js
    Here are the final touches before testing. The usual stuff like error
    handler, importing needed stuff and exporting the app itself
    
    I previously forgot to export router from routes/api/users.js so now I fixed that
    and also added the error handler for unknown routes.
    
    this concludes the development. the next thing in line is testing with
    POSTMAN

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authController = require('../../controllers/authController');
-const authenticateUser= require('../middleware/auth');
+const authenticateUser = require('../../middleware/auth.js');
 const usersController = require('../../controllers/usersController');
 
 const router = express.Router();
@@ -13,17 +13,24 @@ router.post('/login', authController.login);
 
 
 
-//http://localhost:3000/api/tasks?page=1&limit=10
-router.get('/tasks',authenticateUser, usersController.showTasks);
+//http://localhost:3000/api/api/tasks?page=1&limit=10
+router.get('/api/api/tasks',authenticateUser, usersController.showTasks);
 
-//http://localhost:3000/api/tasks
-router.post('/tasks',authenticateUser, usersController.createTask); // create a new task
+//http://localhost:3000/api/api/tasks
+router.post('/api/tasks',authenticateUser, usersController.createTask); // create a new task
 
-//http://localhost:3000/api/tasks/id
-router.put('/tasks/:id', authenticateUser, usersController.updateTask); // update task. we can either 
+//http://localhost:3000/api/api/tasks/id
+router.put('/api/tasks/:id', authenticateUser, usersController.updateTask); // update task. we can either 
                                                     // complete the whole task or change the description
-//http://localhost:3000/api/tasks/id
-router.delete('/tasks/:id',authenticateUser, usersController.deleteTask);
+//http://localhost:3000/api/api/tasks/id
+router.delete('/api/tasks/:id',authenticateUser, usersController.deleteTask);
 
+router.use((req, res, next) => {
+    const unknownMethod = req.method;       
+    const unknownPath = req.path;       
 
+    res.status(500).json({'message': `Nepoznat zahtev ${unknownMethod} ka ${unknownPath}!`});
+});
+
+module.exports = router;
 
