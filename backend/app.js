cc4cd14a7ffde01f740af9f678a01c6b3275236f
Blicko <samardzicluka@proton.me>
2025-03-31 01:57:13 +0200

    Wrote app.js
    Here are the final touches before testing. The usual stuff like error
    handler, importing needed stuff and exporting the app itself
    
    I previously forgot to export router from routes/api/users.js so now I fixed that
    and also added the error handler for unknown routes.
    
    this concludes the development. the next thing in line is testing with
    POSTMAN

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const connectDB  = require('./config/db.js');
+const {urlencoded, json} = require('body-parser');
+const usersRoutes = require('./routes/api/users');
+const rateLimit = require('express-rate-limiter');
 
 const app = express();
 
+app.use(json());
+app.use(urlencoded({extended: false}));
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 5,
+});
+
+app.use(limiter);
+app.use('/api', usersRoutes);
+
+//
+app.use(function (error, req, res, next){
+    const statusCode = error.status || 500;
+
+    res.status(statusCode).json({
+        error:{
+            message: error.message,
+            status: statusCode,
+            stack: error.stack,
+        },
+    });
+});
+
+app.use(function (req,res,next){
+    const error = new Error('Zahtev nije podrzan');
+    error.status = 405;
+    
+    next(error)
+})
 connectDB();
 
 module.exports = app;
